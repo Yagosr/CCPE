ad48e583e44af54079d1d4d15916776c75a7df57
Wesley Morais de Oliveira <f0fp1252@t-systems.com.br>
2023-07-29 14:12:56 -0300

    adição musica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center mt-24 ">
 
-      <audio src="./audio/musica.mp3" autoPlay loop></audio>
+<audio src='./audio/louvorCcpe.mp3' autoPlay loop></audio>
 
-      <video className="fixed opacity-60 h-full w-full mb-20 mr-72 " autoPlay loop muted>
+      <video className="fixed opacity-60 h-full w-full mb-20 mr-72 sm:w-full h-full" autoPlay loop muted>
         <source className="w-full h-full mdh-96" src="./video/ccpeGif.mp4" type="video/mp4" />
       </video>
 
@@ -38,17 +38,15 @@ export default function Home() {
                 <span className="ml-1"> Carta convite</span>
                 <span className="ml-2">Clique aqui</span>
               </div>
-
-              <a href="./arquivos/CartaDaniel.pdf" download="arquivo">
+              <Link href="./arquivos/CartaDaniel.pdf">
                 <div className="flex justify-center items-center cursor-pointer">
                   <div className="flex justify-center items-center text-secondary-100 hover:text-primary-200 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2" /><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" /></svg>
                   </div>
-                </div>  
-              </a>
-              <Link href="./arquivos/CartaDaniel.pdf">
-                     
+                </div>         
               </Link>
+              {/* <a href="./arquivos/CartaDaniel.pdf" download="arquivo">
+              </a> */}
             </div>
             <div>
               <div className="flex flex-col items-center justify-center text-xs sm:text-lg">
